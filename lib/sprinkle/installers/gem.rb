63441cb192231e2d22c8af2bf93dbeed0ba98f38
David L. Kaplan <dave@enchanted.(none)>
2010-10-15 13:54:10 -0400

    added rvm option to gem installer

--- lib/sprinkle/installers/gem.rb
+++ lib/sprinkle/installers/gem.rb
@@ -49,7 +49,9 @@ module Sprinkle
         # rubygems 0.9.5+ installs dependencies by default, and does platform selection
 
         def install_commands #:nodoc:
-          cmd = "gem install #{gem}"
+          cmd =  "echo 'bypass sudo' && "
+          cmd << 'rvmsudo ' if rvm
+          cmd << "gem install #{gem}"
           cmd << " --version '#{version}'" if version
           cmd << " --source #{source}" if source
           cmd << " --install-dir #{repository}" if option?(:repository)

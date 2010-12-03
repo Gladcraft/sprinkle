62860ec246ad6a74d0073ee363f96fae261a2c0b
David L. Kaplan <dave@enchanted.(none)>
2010-12-03 11:35:29 -0500

    added rvm_trusts verifier

--- lib/sprinkle/verifiers/rvm.rb
+++ lib/sprinkle/verifiers/rvm.rb
@@ -15,6 +15,11 @@ module Sprinkle
       def has_rvm_ruby(ruby_version)
         @commands << "echo 'bypass sudo'; rvm list | grep #{ruby_version}"
       end
+
+      # Checks to see if the .rvmrc file in the specified directory is trusted by rvm
+      def rvm_trusts(dir)
+        @commands << "echo 'bypass sudo'; rvm rvmrc trusted #{dir}"
+      end
     end
   end
 end
